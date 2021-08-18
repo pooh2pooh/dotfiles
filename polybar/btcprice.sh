89974d801d8cbad699d4dcef792087dade0c6107
Pooh <pooh@poohprod.ru>
2021-08-18 15:02:07 +0300

    fix usd and btc module for polybar, added dunst config

--- polybar/btcprice.sh
+++ polybar/btcprice.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# CONFIG HERE VAR!
+file=$PWD/dotfiles/polybar/btc.tmp
+
+if [[ -f $file ]];
+  then BTC=$(cat $file)
+  else BTC="..." # if not defiened!
+fi
 DATA=$(curl -s 'https://blockchain.info/ru/ticker' | jq -r '.RUB .buy')
 if ping -c1 ya.ru &> /dev/null;
-  then echo $DATA | cut -f1 -d. | numfmt --to=si
-  else echo "0.0"
+  then echo $DATA | cut -f1 -d. | numfmt --to=si > $file
 fi
+echo $BTC
 #printf "%'.2f\n" $DATA
--- polybar/usdprice.sh
+++ polybar/usdprice.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# CONFIG HERE VAR!
+file=$PWD/dotfiles/polybar/usd.tmp
+
+if [[ -f $file ]];
+  then USD=$(cat $file)
+  else USD="..." # if not defiened!
+fi
 DATA=$(curl -s 'https://www.cbr-xml-daily.ru/daily_json.js' | jq -r '.Valute.USD.Value')
 if ping -c1 ya.ru &> /dev/null;
-  then echo $DATA | cut -c1-5
-  else echo "0.0"
+  then echo $DATA | cut -c1-5 > $file
 fi
+echo $USD
 #printf "%s\n" $DATA
