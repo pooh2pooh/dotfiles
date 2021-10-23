dbb93caef398035366f6d8060ed80423c2775ee7
Pooh <pooh@poohprod.ru>
2021-10-23 15:11:22 +0300

    last updates <3

--- polybar/arch_updates.sh
+++ polybar/arch_updates.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+re='^[0-9]+$'
+
 if ping -c1 ya.ru &> /dev/null
   then updates=$(yay -Qqu | wc -l)
   else updates=0
 fi
 
-if [ $updates -gt 0 ]; then
+if [ $updates -gt 0 ] && [[ $updates =~ $re ]]; then
     echo " $updates"
 else
     echo ""
